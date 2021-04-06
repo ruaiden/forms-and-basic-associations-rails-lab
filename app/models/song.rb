1053e3b518b3053e78692efaf735bb8bc7dee4b7
Aiden <aiden.marques94@gmail.com>
2021-04-05 22:00:49 -0400

    commit tree a06a2d995f958d65183f762a33a7a464d2493733

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,34 @@
 class Song < ActiveRecord::Base
   # add associations here
-end
+  has_many :notes
+  belongs_to :genre
+  belongs_to :artist 
+
+  def genre_name(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end 
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end 
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if content.strip != ""
+         self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end 
+end 
